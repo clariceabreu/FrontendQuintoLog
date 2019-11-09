39b7020fdd0e5f4f0970b42b8ce0bba51aaab776
Camila Araujo <mila.ag20@gmail.com>
2019-11-09 14:48:47 -0300

    remove variable user

--- src/utils.js
+++ src/utils.js
@@ -4,9 +4,10 @@ import 'moment/locale/pt-br';
 
 //const user = useSelector(state => state.authentication);
 
+
 const Axios = axios.create({
     baseURL: 'http://localhost:8181/',
-    headers: {'Authorization': 'barear '+ user.token}
+    headers: {'Authorization': 'barear '} //+ user.token}
 });
 
 export { Axios };
